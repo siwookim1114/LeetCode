a5a2b424eb870c09365a375d0d6d54ba5db9058f
siwookim1114 <63380176+siwookim1114@users.noreply.github.com>
2024-07-09 02:18:01 +0900

    Update 0125-valid-palindrome.py
    Added second attempt solution with different algorithm

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,3 +1,15 @@
+## Initial Solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr = []
+        s = s.lower()
+        for char in s:
+            if char.isalnum():
+                arr.append(char)
+        return arr[:] == arr[::-1]
+
+
+## Second attempt solution
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
@@ -5,6 +17,11 @@ class Solution:
         s = re.sub('[^a-z0-9]', '', s)   ## replace characters that are NOT a-z or 0-9 with ''
         return s == s[::-1]  ## slicing
     
+
+"""
+Original algorithm was using list comprehension and use if statements to only receive characters that are alphanumerical. However, instead of using for loops and if statements, I 
+thought of using regex to filter out non-alphanumericals using re.sub(). Worked out better. 
+"""
 
 
 
