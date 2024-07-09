79f683c1728d3fafb749b129aabf3a00811d92ee
siwookim1114 <63380176+siwookim1114@users.noreply.github.com>
2024-07-10 04:14:17 +0900

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,3 +6,11 @@ class Solution:
             if new in store:
                 return [key, store[new]]
             store[value] = key
+
+"""
+Analysis: Instead of using brute force which has a O(n^2) time complexity, by using a dictionary hash table, it could reduce the time complexity to O(1) or O(n) in the worst case. Hence by declaring a dictionary
+and storing the key, value inside the dictionary by {value:index}, I could declare a new target by target - value, and search inside the dictionary for the corresponding value right away. If not inside, add it on to the
+dictionary. 
+
+Resulted in very high time complexity. 
+"""
