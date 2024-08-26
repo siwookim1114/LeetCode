ce7991d032fdf293e00b40ca172a0ea99849a5ea
siwookim1114 <63380176+siwookim1114@users.noreply.github.com>
2024-08-27 02:00:26 +0900

    Time: 29 ms (90.66%), Space: 16.6 MB (24.55%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,17 +1,16 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewels_set = set()
+        stones_set = defaultdict(int)
         for item in jewels:
             jewels_set.add(item)
-        
+        for stone in stones:
+            stones_set[stone] +=1
+            
         cnt = 0
-        for ele in stones:
-            if ele in jewels:
-                cnt +=1
+        for key in stones_set.keys():
+            if key in jewels_set:
+                cnt += stones_set[key]
         
         return cnt
-            
-        
-        
-        
-        
+                
